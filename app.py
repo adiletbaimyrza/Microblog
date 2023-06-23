3fcdec81aa6a9fd3f23e13654538f3f8a1e01dd0
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-06-23 15:30:23 +0200

    second init

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        entry_content = request.form.get('content')
+        print(entry_content)
+    else:
+        return render_template('home.html')
